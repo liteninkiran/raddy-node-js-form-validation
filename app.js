39bdd1e05f15d94352cac6e3372f908d2b35891b
Kiran Anand <liteninkiran@hotmail.com>
2023-07-26 13:47:57 +0100

    Add endpoints

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 // Imports
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 5000;
+
+// Set Templating Enginge
+app.set('view engine', 'ejs')
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.get('', (req, res)=> {
+    res.render('index')
+})
+
+app.get('/register', (req, res)=> {
+    res.render('register')
+})
 
 // Listen on Port 5000
-app.listen(port, () => console.info(`App listening on port ${port}`))
+app.listen(port, () => console.info(`App listening on port ${port}`));
